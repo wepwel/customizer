168c764d1a715aed510d5fa9f5742fbc90c89936
wepwel <info@wepwel.com>
2025-05-08 10:18:08 +0200

    a

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ document.querySelector('#sideMenu button:nth-child(5)').addEventListener('click'
 });
 
 
-window.addEventListener('DOMContentLoaded', () => {
+/*window.addEventListener('DOMContentLoaded', () => {
   const fakeMessage = {
     backgroundImage: "https://static.wixstatic.com/media/a0452a_f33e912885e34c1c8e578acf556c55fe~mv2.webp",
     designs: [
@@ -129,4 +129,4 @@ window.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     window.postMessage(fakeMessage, "*");
   }, 500);
-});
+});*/
